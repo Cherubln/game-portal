c2d87d2aa2dff06b11a2fbe5d583a1c65353d02d
Cherubin Ndikubwayo <cherubinkubwayo@gmail.com>
2023-12-16 09:26:23 +0200

    Show a spinner fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { isLoading, error, data } = useGenres();
 
+  if (isLoading) return <Spinner />;
+  if (error) return null;
   return (
     <List>
       {data.map((genre) => (
