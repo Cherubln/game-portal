c765dc810b068e195511004b0b48fd8d6afeba08
Cherubin Ndikubwayo <cherubinkubwayo@gmail.com>
2023-12-18 10:46:53 +0200

    Update app logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,35 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import { Box, HStack, Image, Text } from "@chakra-ui/react";
 import ToggleTheme from "./ToggleTheme";
 import SearchInput, { SearchProps } from "./SearchInput";
+import cirlceWrapper from "../assets/logoWrapper.svg";
 
 const NavBar = ({ onSearch }: SearchProps) => {
   return (
     <HStack>
-      <Image src={logo} boxSize="60px" />
+      <Box
+        position="relative"
+        bgClip="text"
+        bgGradient="linear(to-r, orange.700, orange.600, orange.400)"
+        color={"transparent"}
+      >
+        <Text
+          fontWeight={"bold"}
+          fontSize={"2xl"}
+          padding={3}
+          whiteSpace={"nowrap"}
+        >
+          Game Hub
+        </Text>
+        <Image
+          src={cirlceWrapper}
+          alt="circle"
+          boxSize={"100%"}
+          position="absolute"
+          left={0}
+          top={0}
+          height={"100%"}
+        />
+      </Box>
       <SearchInput onSearch={onSearch} />
       <ToggleTheme />
     </HStack>
